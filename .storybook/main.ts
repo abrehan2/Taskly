5ea1d0a0e48d9919a4a9f7f125d16cee71e1fd85
abrehan2 <rehannajam2@gmail.com>
2024-08-11 16:19:30 +0500

    build: implement storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  staticDirs: ['..\\public'],
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
+
+export default config;
